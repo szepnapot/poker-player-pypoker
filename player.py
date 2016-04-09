84a60b464537dba600901801ffac2beebb212d35
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-04-09 14:12:42 +0200

    pairs

--- player.py
+++ player.py
@@ -10,13 +10,16 @@ def warning(*objs):
 
 class Player:
     VERSION = "Default Python folding player"
-    
+   
+    def preFlopBet(self):
+       if self.state.get_cards()[0]["rank"] == self.state.get_cards()[1]["rank"]:
+          return 500
+       else:
+          return 0
+ 
     def calcBet(self):
         if self.state.get_round() == 0:
-            if "K" or "A" or "J" or "Q" or "A" in self.state.get_rank():
-                return 500
-            else:
-                return 0
+            return self.preFlopBet()
         else:
             return self.state.buy_in() - self.state.player()['bet']
 
