4e5048b92a1c581d485558e68c423219c2be8a08
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-04-09 10:00:33 +0200

    returning 240

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 1
+        return 240
 
     def showdown(self, game_state):
         pass
