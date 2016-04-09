5619cf86860f5d7ab17a7de57f932d860616c005
Peter <silentpocok@gmail.com>
2016-04-09 12:55:04 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import sys
-
+from game_state import GameState
 
 def warning(*objs):
     print("WARNING: ", *objs, file=sys.stderr)
@@ -8,36 +8,12 @@ def warning(*objs):
 
 class Player:
     VERSION = "Default Python folding player"
-
-    def in_action_number(self, game_state):
-        return game_state["in_action"]
-
-    def player(self, game_state):
-        return game_state["players"][self.in_action_number( game_state)]
-
-    def get_cards(self, game_state):
-        return game_state[self.player( game_state)]["hole_cards"]
-
-    def get_stack(self, game_state):
-        return game_state[self.player( game_state)]["stack"]
-
-    def is_dealer(self, game_state):
-        if game_state["in_action"]["dealer"] == 1:
-            return True
-        return False
-
-    def community_cards(self, game_state):
-        return game_state["in_action"]["community_cards"]
-
-    def buy_in(self, game_state):
-        return ""
+    
 
     def betRequest(self, game_state):
+        state = GameState(game_state)
         try:
-            hand = self.get_cards( game_state )
-            current_stack = self.get_stack( game_state )
-            if "K" or "A" or "J" or "Q" or "A" in hand["rank"]:
-                return 600
+            return state.bet()
         except:
             return 500
 
