5ffb929c744e34f94b9e5cd99cb581b99223541f
Peter <silentpocok@gmail.com>
2016-04-09 14:06:52 +0200

    Update player.py

--- player.py
+++ player.py
@@ -10,15 +10,17 @@ def warning(*objs):
 
 class Player:
     VERSION = "Default Python folding player"
-    
+
     def calcBet(self):
-        if self.state.get_round() == 0:
-            if "K" or "A" or "J" or "Q" or "A" in self.state.get_rank():
+        if "K" or "A" or "J" or "Q" or "A" in self.state.get_rank():
+            if self.state.get_round() == 0:
                 return 500
-            else:
+            elif self.state.get_round() == 1:
+                if self.state.get_cards()[0] or self.state.get_cards()[1] in self.state.community_cards():
+                    return self.state.buy_in()
                 return 0
         else:
-            return self.state.buy_in() - self.state.player()['bet']
+            return self.state.buy_in(self.state) - self.state.player()['bet']
 
     def betRequest(self, game_state):
         self.state = GameState(game_state)
@@ -26,8 +28,7 @@ class Player:
             return self.calcBet()
         except Exception as x:
             warning("Exception during betRequest", x)
-            return 501
+            return 500
 
     def showdown(self, game_state):
         pass
-
