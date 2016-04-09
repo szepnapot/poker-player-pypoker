0da9604d5814d0347d10b05ed7131426f57e414a
Ritchie Vasko <vaskorichard@gmail.com>
2016-04-09 12:01:32 +0200

    Replacing tabs with spaces.

--- test_player.py
+++ test_player.py
@@ -4,11 +4,14 @@ from game_state import GameState
 #to run the test type into the command line:
 # $ python -m unittest test_player
 
-class TestPlayer(TestCase):
-	def setUp(self):
-		self.player = Player()
-		self.state = GameState({'in_action':666})
+#class GameStateMock( GameState ):
 
-	def test_bet(self):
-		self.assertEqual(500,self.player.betRequest({'in_action':666}))
+
+class TestPlayer(TestCase):
+   def setUp(self):
+      self.player = Player()
+      self.state = GameState({'in_action':666})
+
+   def test_bet(self):
+      self.assertEqual(500,self.player.betRequest({'in_action':666}))
 
